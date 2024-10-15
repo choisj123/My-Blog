93632c53e8f2e87bd875da5f7c3b4cb57aef58c4
choisj123 <98chltnwls@gmail.com>
2024-10-15 19:03:23 +0900

    배포 관련 수정

--- my-blog/app/api/comment/route.js
+++ my-blog/app/api/comment/route.js
@@ -1,9 +1,8 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "../../lib/prisma";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]/route";
 
-const prisma = new PrismaClient();
 export async function POST(request) {
   const session = await getServerSession(authOptions);
   if (!session) {
